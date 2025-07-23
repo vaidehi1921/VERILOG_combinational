81e60c78eed1d5c5e4ec4173d14ca052b71641f4
Vaidehi Goswami <vago1921@gmail.com>
2025-07-23 10:45:07 +0530

    Update connecting_ports.v

--- connecting_ports.v
+++ connecting_ports.v
@@ -4,18 +4,13 @@ BY name
 
 module top_module ( input a,b,c,d, output out1, out2);
 mod_a inst( .in1(a), .in2(b), .in3(c), .in4(d), .out1(out1), .out2(out2));
-
 endmodule
-
-
 
 BY position
 
 module top_module ( input a,b,c,d, output out1, out2);
 mod_a inst(a, b, c, d , out1, out2);
-
 endmodule
-
 
 
 /* shift register of length 3 */
